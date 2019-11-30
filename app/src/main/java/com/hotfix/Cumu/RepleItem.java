b6b0dc2f185402ec3bcd55aaac72056c76843c0b
Gwanwoo-Park <49912860+Gwanwoo-Park@users.noreply.github.com>
2019-11-30 19:43:20 +0900

    Create RepleItem.java

---
+++ app/src/main/java/com/hotfix/Cumu/RepleItem.java
@@ -0,0 +1,27 @@
+package com.hotfix.Cumu;
+
+public class RepleItem {
+    String content;
+    String name;
+
+    public RepleItem(String name, String content) {
+        this.content = content;
+        this.name = name;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName() {
+        this.name = name;
+    }
+}
